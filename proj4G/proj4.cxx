b26fa9b9819e55e18f2e159c0b5caad7ebdc1036
ayenpure <abhishek@uoregon.edu>
2018-02-05 14:26:19 -0800

    Adding project 4, 4G

---
+++ proj4G/proj4.cxx
@@ -0,0 +1,578 @@
+#include <cmath>
+
+#include <vtkImageData.h>
+#include <vtkPNGWriter.h>
+#include <vtkPointData.h>
+#include <vtkCellArray.h>
+#include <vtkPoints.h>
+#include <vtkUnsignedCharArray.h>
+#include <vtkFloatArray.h>
+#include <vtkDataSetReader.h>
+#include <vtkRectilinearGrid.h>
+#include <vtkFloatArray.h>
+#include <vtkPolyData.h>
+#include <vtkDataSetWriter.h>
+#include <vtkTubeFilter.h>
+#include <vtkPolyDataNormals.h>
+#include <vtkSphereSource.h>
+
+#include <vtkCamera.h>
+#include <vtkDataSetMapper.h>
+#include <vtkRenderer.h>
+#include <vtkActor.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkSmartPointer.h>
+
+// ****************************************************************************
+//  Function: GetNumberOfPoints
+//
+//  Arguments:
+//     dims: an array of size 3 with the number of points in X, Y, and Z.
+//           2D data sets would have Z=1
+//
+//  Returns:  the number of points in a rectilinear mesh
+//
+// ****************************************************************************
+
+int GetNumberOfPoints(const int *dims)
+{
+    // 3D
+    //return dims[0]*dims[1]*dims[2];
+    // 2D
+    return dims[0]*dims[1];
+}
+
+// ****************************************************************************
+//  Function: GetNumberOfCells
+//
+//  Arguments:
+//
+//      dims: an array of size 3 with the number of points in X, Y, and Z.
+//            2D data sets would have Z=1
+//
+//  Returns:  the number of cells in a rectilinear mesh
+//
+// ****************************************************************************
+
+int GetNumberOfCells(const int *dims)
+{
+    // 3D
+    //return (dims[0]-1)*(dims[1]-1)*(dims[2]-1);
+    // 2D
+    return (dims[0]-1)*(dims[1]-1);
+}
+
+
+// ****************************************************************************
+//  Function: GetPointIndex
+//
+//  Arguments:
+//      idx:  the logical index of a point.
+//              0 <= idx[0] < dims[0]
+//              1 <= idx[1] < dims[1]
+//              2 <= idx[2] < dims[2] (or always 0 if 2D)
+//      dims: an array of size 3 with the number of points in X, Y, and Z.
+//            2D data sets would have Z=1
+//
+//  Returns:  the point index
+//
+// ****************************************************************************
+
+int GetPointIndex(const int *idx, const int *dims)
+{
+    // 3D
+    //return idx[2]*dims[0]*dims[1]+idx[1]*dims[0]+idx[0];
+    // 2D
+    return idx[1]*dims[0]+idx[0];
+}
+
+
+// ****************************************************************************
+//  Function: GetCellIndex
+//
+//  Arguments:
+//      idx:  the logical index of a cell.
+//              0 <= idx[0] < dims[0]-1
+//              1 <= idx[1] < dims[1]-1 
+//              2 <= idx[2] < dims[2]-1 (or always 0 if 2D)
+//      dims: an array of size 3 with the number of points in X, Y, and Z.
+//            2D data sets would have Z=1
+//
+//  Returns:  the cell index
+//
+// ****************************************************************************
+
+int GetCellIndex(const int *idx, const int *dims)
+{
+    // 3D
+    //return idx[2]*(dims[0]-1)*(dims[1]-1)+idx[1]*(dims[0]-1)+idx[0];
+    // 2D
+    return idx[1]*(dims[0]-1)+idx[0];
+}
+
+// ****************************************************************************
+//  Function: GetLogicalPointIndex
+//
+//  Arguments:
+//      idx (output):  the logical index of the point.
+//              0 <= idx[0] < dims[0]
+//              1 <= idx[1] < dims[1] 
+//              2 <= idx[2] < dims[2] (or always 0 if 2D)
+//      pointId:  a number between 0 and (GetNumberOfPoints(dims)-1).
+//      dims: an array of size 3 with the number of points in X, Y, and Z.
+//            2D data sets would have Z=1
+//
+//  Returns:  None (argument idx is output)
+//
+// ****************************************************************************
+
+void GetLogicalPointIndex(int *idx, int pointId, const int *dims)
+{
+    // 3D
+    // idx[0] = pointId%dim[0];
+    // idx[1] = (pointId/dims[0])%dims[1];
+    // idx[2] = pointId/(dims[0]*dims[1]);
+
+    // 2D
+    idx[0] = pointId%dims[0];
+    idx[1] = pointId/dims[0];
+}
+
+
+// ****************************************************************************
+//  Function: GetLogicalCellIndex
+//
+//  Arguments:
+//      idx (output):  the logical index of the cell index.
+//              0 <= idx[0] < dims[0]-1
+//              1 <= idx[1] < dims[1]-1 
+//              2 <= idx[2] < dims[2]-1 (or always 0 if 2D)
+//      cellId:  a number between 0 and (GetNumberOfCells(dims)-1).
+//      dims: an array of size 3 with the number of points in X, Y, and Z.
+//            2D data sets would have Z=1
+//
+//  Returns:  None (argument idx is output)
+//
+// ****************************************************************************
+
+void GetLogicalCellIndex(int *idx, int cellId, const int *dims)
+{
+    // 3D
+    // idx[0] = cellId%(dims[0]-1);
+    // idx[1] = (cellId/(dims[0]-1))%(dims[1]-1);
+    // idx[2] = cellId/((dims[0]-1)*(dims[1]-1));
+
+    // 2D
+    idx[0] = cellId%(dims[0]-1);
+    idx[1] = cellId/(dims[0]-1);
+}
+
+
+// ****************************************************************************
+//  Function: EvaluateVectorFieldAtLocation
+//
+//  Arguments:
+//     pt: a two-dimensional location
+//     dims: an array of size two.  
+//              The first number is the size of the array in argument X, 
+//              the second the size of Y.
+//     X: an array (size is specified by dims).  
+//              This contains the X locations of a rectilinear mesh.
+//     Y: an array (size is specified by dims).  
+//              This contains the Y locations of a rectilinear mesh.
+//     F: a vector field defined on the mesh.  Its size is 2*dims[0]*dims[1].
+//        The first value in the field is the x-component for the first point.
+//        The second value in the field is the y-component for the first point.
+//
+//     rv (output): the interpolated field value. (0,0) if the location is out of bounds.
+//
+// ****************************************************************************
+
+void EvaluateVectorFieldAtLocation(const float *pt, const int *dims, const float *X, 
+                              const float *Y, const float *F, float *rv)
+{
+    rv[0] = 0; // setting the x-component of the velocity
+    rv[1] = 0; // setting the y-component of the velocity
+
+    int logicalPointIndex[2] = {-1, -1};
+    for (int i = 0; i < dims[0] - 1; i++)
+        if(X[i] <= pt[0] && X[i+1] > pt[0])
+            logicalPointIndex[0] = i;
+    for (int i = 0; i < dims[1] - 1; i++)
+        if(Y[i] <= pt[1] && Y[i+1] > pt[1])
+            logicalPointIndex[1] = i;
+    if(logicalPointIndex[0] == -1 || logicalPointIndex[1] ==  -1)
+        return;
+    int xInd = logicalPointIndex[0];
+    int yInd = logicalPointIndex[1];
+    /*
+       3 ---- 4
+       |      |  We have point 1, point 2 is +1 in X.
+       |      |  Point 3 is + 1 in Y and Point 4 +1 in X and Y.
+       1 ---- 2
+    */
+    int point1 = GetPointIndex(logicalPointIndex, dims);
+    ++logicalPointIndex[0];
+    int point2 = GetPointIndex(logicalPointIndex, dims);
+    ++logicalPointIndex[1];
+    int point4 = GetPointIndex(logicalPointIndex, dims);
+    --logicalPointIndex[0];
+    int point3 = GetPointIndex(logicalPointIndex, dims);
+
+    float propX = (float)(pt[0] - X[xInd]) / (float)(X[xInd + 1] - X[xInd]);
+    float propY = (float)(pt[1] - Y[yInd]) / (float)(Y[yInd + 1] - Y[yInd]);
+
+    // F has 2 components now.
+    // Multiply index by 2 to get correct location : loc.
+    // loc[0] -> x component of velocity.
+    // loc[1] -> y component of velocity.
+
+    int loc1, loc2;
+
+    // Interpolation in X
+    float velocity1[2];
+    loc1 = point1*2;
+    loc2 = point2*2;
+    velocity1[0] = F[loc1] + propX * (F[loc2] - F[loc1]);
+    velocity1[1] = F[loc1+1] + propX * (F[loc2+1] - F[loc1+1]);
+
+    float velocity2[2];
+    loc1 = point3*2;
+    loc2 = point4*2;
+    velocity2[0] = F[loc1] + propX * (F[loc2] - F[loc1]);
+    velocity2[1] = F[loc1+1] + propX * (F[loc2+1] - F[loc1+1]);
+
+    // Interpolation in Y
+    rv[0] = velocity1[0] + propY * (velocity2[0] - velocity1[0]);
+    rv[1] = velocity1[1] + propY * (velocity2[1] - velocity1[1]);
+}
+
+// ****************************************************************************
+//  Function: AdvectWithRK4Step
+//
+//  Arguments:
+//     pt: the seed location (two-dimensions)
+//     dims: an array of size two.  
+//              The first number is the size of the array in argument X, 
+//              the second the size of Y.
+//     X: an array (size is specified by dims).  
+//              This contains the X locations of a rectilinear mesh.
+//     Y: an array (size is specified by dims).  
+//              This contains the Y locations of a rectilinear mesh.
+//     F: a vector field defined on the mesh.  Its size is 2*dims[0]*dims[1].
+//     h: The size of the Euler step
+//     nsteps: The number of Euler steps to take
+//     output_locations (output): An array of size 2*(nsteps+1).  It's first entry
+//        should be the seed location.  The second entry should be the result
+//        of the first advection step.  The final entry should be the result
+//        of the final advection step.
+//     speeds (output): An array of size (nsteps+1).  It's first entry should be the
+//        speed at the seed location.  It's final entry should be the speed
+//        at the location of the result of the final advection step.
+//        Recall that speed is the magnitude of the velocity.
+//
+// ****************************************************************************
+
+template <typename T>
+inline void
+scaleVector(T scaleVal, T* vector)
+{
+  vector[0] = scaleVal * vector[0];
+  vector[1] = scaleVal * vector[1];
+}
+
+template <typename T>
+inline T
+vectorMagnitude(T* vector)
+{
+  return sqrt( (vector[0] * vector[0])
+               + (vector[1] * vector[1]));
+}
+
+template <typename S, typename D>
+inline void
+copyVector(S* source, D* destination,int src_offset,int dest_offset)
+{
+  destination[dest_offset] = source[src_offset];
+  destination[dest_offset+1] = source[src_offset+1];
+}
+
+template <typename P, typename V>
+void
+AdvectWithEulerStep(P *pt, V *velocity, const int *dims, const float *X,
+                    const float *Y, const float *F, float h, float *outputPos)
+{
+  float displacement[2];
+  copyVector(velocity, displacement, 0, 0);
+  scaleVector(h, displacement);
+  //Velocity now is actually displacement in X and Y
+  outputPos[0] = pt[0] + displacement[0];
+  outputPos[1] = pt[1] + displacement[1];
+}
+
+void
+AdvectWithRK4Step(const float *pt, const int *dims, const float *X,
+                  const float *Y, const float *F,
+                  float h, int nsteps, float *output_locations, float *speeds)
+{
+    for(int step = 0; step <= nsteps; step++)
+    {
+     if(step == 0)
+        copyVector(pt,output_locations,0,step*2);
+      else
+      {
+        float velocity[2], tempPos[2],
+              velocity1[2], velocity2[2],
+              velocity3[2], velocity4[2],
+              currentPos[2], outputPos[2];
+
+        copyVector(output_locations, currentPos, (step-1)*2, 0);
+
+        // K1 : velocity1
+        EvaluateVectorFieldAtLocation(currentPos, dims, X, Y, F, velocity1);
+        // K2 : velocity2, step with velocity1
+        AdvectWithEulerStep(currentPos, velocity1, dims, X, Y, F, h/2.0f, tempPos);
+        EvaluateVectorFieldAtLocation(tempPos, dims, X, Y, F, velocity2);
+        // K3 : velocity3, step with velocity2
+        AdvectWithEulerStep(currentPos, velocity2, dims, X, Y, F, h/2.0f, tempPos);
+        EvaluateVectorFieldAtLocation(tempPos, dims, X, Y, F, velocity3);
+        // K4 : velocity4, step with velocity3
+        AdvectWithEulerStep(currentPos, velocity3, dims, X, Y, F, h, tempPos);
+        EvaluateVectorFieldAtLocation(tempPos, dims, X, Y, F, velocity4);
+        // Calculate velocity for final step.
+        velocity[0] = velocity1[0] + 2* velocity2[0] + 2*velocity3[0] + velocity4[0];
+        velocity[1] = velocity1[1] + 2* velocity2[1] + 2*velocity3[1] + velocity4[1];
+
+        scaleVector(1.0f/6.0f, velocity);
+        // Take Final step.
+        AdvectWithEulerStep(currentPos, velocity, dims, X, Y, F, h, outputPos);
+
+        speeds[step] = vectorMagnitude(velocity);
+        copyVector(outputPos, output_locations, 0, step*2);
+      }
+    }
+}
+
+// ****************************************************************************
+//  Function: CalculateArcLength
+//
+//  Arguments:
+//     locations: an array of 2D locations.
+//     nlocations: the number of locations in the array "locations".
+//
+//  Returns: the arc length, meaning the distance between each successive
+//           pair of points
+//
+// ****************************************************************************
+template<typename P1, typename P2>
+inline float
+calculateDistance(P1* p1, P2* p2)
+{
+  float xdiff = p1[0] - p2[0];
+  float ydiff = p1[1] - p2[1];
+  xdiff *= xdiff;
+  ydiff *= ydiff;
+  return sqrt(xdiff + ydiff);
+}
+
+float
+CalculateArcLength(const float *output_locations, int nlocations)
+{
+    float length = 0.0f;
+    for(int loc = 0; loc < nlocations - 1; loc++)
+      length += calculateDistance(&output_locations[loc*2],
+                                  &output_locations[(loc+1)*2]);
+    return length;
+}
+
+void
+WriteImage(vtkImageData *img, const char *filename)
+{
+    std::string full_filename = filename;
+    full_filename += ".png";
+    vtkPNGWriter *writer = vtkPNGWriter::New();
+    writer->SetInputData(img);
+    writer->SetFileName(full_filename.c_str());
+    writer->Write();
+    writer->Delete();
+}
+
+vtkImageData *
+NewImage(int width, int height)
+{
+    vtkImageData *image = vtkImageData::New();
+    image->SetDimensions(width, height, 1);
+    //image->SetWholeExtent(0, width-1, 0, height-1, 0, 0);
+    //image->SetUpdateExtent(0, width-1, 0, height-1, 0, 0);
+    //image->SetNumberOfScalarComponents(3);
+    image->AllocateScalars(VTK_UNSIGNED_CHAR, 3);
+    //image->AllocateScalars();
+
+    return image;
+}
+
+// VTK files are only 3D, so the vector data is all of the form (X,Y,0).
+// Remove the 0's since it is counter-intuitive for students who are 
+// thinking of this as 2D data.
+float *
+Convert3DVectorDataTo2DVectorData(const int *dims, const float *F)
+{
+    float *rv = new float[dims[0]*dims[1]*2];
+    int index3D = 0;
+    int index2D = 0;
+    for (int i = 0 ; i < dims[0] ; i++)
+       for (int j = 0 ; j < dims[1] ; j++)
+       {
+           rv[index2D]   = F[index3D];
+           rv[index2D+1] = F[index3D+1];
+           index2D += 2;
+           index3D += 3;
+       }
+
+    return rv;
+}
+
+vtkPolyData *
+CreateVTKPolyData(int nseeds, int nsteps, float **output_locations, float **speeds)
+{
+    int numPoints = nseeds*(nsteps+1);
+    vtkPoints *pts = vtkPoints::New();
+    pts->SetNumberOfPoints(numPoints);
+    vtkFloatArray *var = vtkFloatArray::New();
+    var->SetName("speed");
+    var->SetNumberOfTuples(numPoints);
+    int ptIdx = 0;
+    vtkCellArray *lines = vtkCellArray::New();
+    lines->EstimateSize(numPoints,2);
+    for (int i = 0 ; i < nseeds ; i++)
+    {
+        for (int j = 0 ; j < nsteps+1 ; j++)
+        {
+            double pt[3];
+            pt[0] = output_locations[i][2*j];
+            pt[1] = output_locations[i][2*j+1];
+            pt[2] = 0.;
+            pts->SetPoint(ptIdx, pt);
+            var->SetTuple1(ptIdx, speeds[i][j]);
+            if (j > 0)
+            {
+                vtkIdType ids[2] = { ptIdx-1, ptIdx };
+                lines->InsertNextCell(2, ids);
+            }
+            ptIdx++;
+        }
+    }
+
+    vtkPolyData *pd = vtkPolyData::New();
+    pd->SetPoints(pts);
+    pd->GetPointData()->AddArray(var);
+    pd->GetPointData()->SetActiveScalars("speed");
+    pd->SetLines(lines);
+    lines->Delete();
+    var->Delete();
+    pts->Delete();
+
+    return pd;
+}
+
+int main()
+{
+    int  i, j;
+
+    vtkDataSetReader *rdr = vtkDataSetReader::New();
+    rdr->SetFileName("proj4_data.vtk");
+    rdr->Update();
+
+    int dims[3];
+    vtkRectilinearGrid *rgrid = (vtkRectilinearGrid *) rdr->GetOutput();
+    rgrid->GetDimensions(dims);
+
+    float *X = (float *) rgrid->GetXCoordinates()->GetVoidPointer(0);
+    float *Y = (float *) rgrid->GetYCoordinates()->GetVoidPointer(0);
+    float *F_3D = (float *) rgrid->GetPointData()->GetVectors()->GetVoidPointer(0);
+    float *F = Convert3DVectorDataTo2DVectorData(dims, F_3D);
+
+    const int npts = 10;
+    float pt[npts][3] =
+         {
+            {10.1119, 1.22062, 0},
+            {8.62376, 13.3839, 0},
+            {1.55026, 1.26123, 0},
+            {6.9736, 0.653565, 0},
+            {2, 2.74117, 0},
+            {8.93699, 10.4111, 0},
+            {6.08791, -0.533753, 0},
+            {10.0543, 1.38024, 0},
+            {3.84128, -0.768977, 0},
+            {6.66757, 6.0259, 0},
+         };
+
+
+    for (i = 0 ; i < npts ; i++)
+    {
+       float vec[2];
+       EvaluateVectorFieldAtLocation(pt[i], dims, X, Y, F, vec);
+       cerr << "Velocity at (" << pt[i][0] <<", "<<pt[i][1] << ") is (" << vec[0] << ", " << vec[1] << ")" << endl;
+    }
+
+    float h = 0.01;
+    const int nsteps = 5000;
+    float **output_locations = new float*[2*(npts+1)];
+    float **speeds = new float*[npts+1];
+    for (i = 0 ; i < npts ; i++)
+    {
+       output_locations[i] = new float[(nsteps+1)*2];
+       speeds[i] = new float[nsteps];
+       AdvectWithRK4Step(pt[i], dims, X, Y, F, h, nsteps, output_locations[i], speeds[i]);
+       float length = CalculateArcLength(output_locations[i], nsteps+1);
+       cerr << "Arc length for (" << pt[i][0] << ", " << pt[i][1] << ") is " << length << endl;
+    }
+
+    vtkPolyData *pd = CreateVTKPolyData(npts, nsteps, output_locations, speeds);
+
+    //This can be useful for debugging
+/*
+    vtkDataSetWriter *writer = vtkDataSetWriter::New();
+    writer->SetFileName("paths.vtk");
+    writer->SetInput(pd);
+    writer->Write();
+ */
+
+    vtkSmartPointer<vtkDataSetMapper> win1Mapper =
+      vtkSmartPointer<vtkDataSetMapper>::New();
+    win1Mapper->SetInputData(pd);
+    win1Mapper->SetScalarRange(0, 0.15);
+  
+    vtkSmartPointer<vtkActor> win1Actor =
+      vtkSmartPointer<vtkActor>::New();
+    win1Actor->SetMapper(win1Mapper);
+  
+    vtkSmartPointer<vtkRenderer> ren1 =
+      vtkSmartPointer<vtkRenderer>::New();
+  
+    vtkSmartPointer<vtkRenderWindow> renWin =
+      vtkSmartPointer<vtkRenderWindow>::New();
+    renWin->AddRenderer(ren1);
+  
+    vtkSmartPointer<vtkRenderWindowInteractor> iren =
+      vtkSmartPointer<vtkRenderWindowInteractor>::New();
+    iren->SetRenderWindow(renWin);
+    ren1->AddActor(win1Actor);
+    ren1->SetBackground(0.0, 0.0, 0.0);
+    renWin->SetSize(800, 800);
+  
+    ren1->GetActiveCamera()->SetFocalPoint(5,5,0);
+    ren1->GetActiveCamera()->SetPosition(5,5,30);
+    ren1->GetActiveCamera()->SetViewUp(0,1,0);
+    ren1->GetActiveCamera()->SetClippingRange(20, 120);
+    ren1->GetActiveCamera()->SetDistance(30);
+
+    // This starts the event loop and invokes an initial render.
+    //
+    iren->Initialize();
+    iren->Start();
+
+    delete [] F;
+    pd->Delete();
+}
