017c32e1676e60151d674409e53745caef89468e
ayenpure <abhishek@uoregon.edu>
2018-03-20 08:07:32 -0700

    adding changes to read file

--- raycasting/auxiliary.hxx
+++ raycasting/auxiliary.hxx
@@ -1,18 +1,39 @@
 #include <cmath>
 #include <iostream>
 
+#define HEIGHT 100
+#define WIDTH 100
+#define PI 3.14159265
+
 template <typename T>
 struct Vec3 {
   T x, y, z;
 
   Vec3(){};
   Vec3(T xVal, T yVal, T zVal) : x(xVal), y(yVal), z(zVal) {}
+
+  Vec3 operator -(Vec3& vector)
+  {
+    return Vec3(x - vector.x, y - vector.y, z - vector.z);
+  }
+
+  Vec3 operator +(Vec3& vector)
+  {
+    return Vec3(x + vector.x, y + vector.y, z + vector.z);
+  }
+
 };
 
 template <typename T>
 void PrintVector(Vec3<T> &vector) {
   std::cout << "{" << vector.x << ", " << vector.y << ", " << vector.z << "}"
             << std::endl;
+}
+
+template <typename T>
+Vec3<T> Multiply(Vec3<T> &vector, T val) {
+  Vec3<T> output = Vec3<T>(vector.x * val,vector.y * val,vector.z * val);
+  return output;
 }
 
 template <typename T>
@@ -23,7 +44,7 @@ T Magnitude(Vec3<T> &vector) {
 }
 
 template <typename T>
-T Normalize(Vec3<T> &vector) {
+void Normalize(Vec3<T> &vector) {
   T magnitude = Magnitude(vector);
   vector.x /= magnitude;
   vector.y /= magnitude;
@@ -31,17 +52,11 @@ T Normalize(Vec3<T> &vector) {
 }
 
 template <typename T>
-void Cross(Vec3<T> &input1, Vec3<T> &input2, Vec3<T> &cross) {
-  Vec3<T> output = (input1.y * input2.z - input1.z * input2.y,
-                    input1.x * input2.z - input1.z * input2.x,
-                    input1.x * input2.y - input1.y * input2.x);
-  cross = output;
-}
-
-template <typename T>
-void Multiply(Vec3<T> &vector, T val, Vec3<T> &product) {
-  Vec3<T> output = (val*vector.x, val*vector.y, val*vector.z);
-  product = output;
+Vec3<T> Cross(Vec3<T> &input1, Vec3<T> &input2) {
+  Vec3<T> output = Vec3<T>(input1.y * input2.z - input1.z * input2.y,
+                           input1.x * input2.z - input1.z * input2.x,
+                           input1.x * input2.y - input1.y * input2.x);
+  return output;
 }
 
 template <typename T>
@@ -66,4 +81,20 @@ Camera<T> SetupCamera(void) {
   camera.far = 1.4e+8;
 
   return camera;
+}
+
+template <typename T>
+void CalculateViewParameters(Camera<T> &camera, Vec3<T> &look, Vec3<T> &forX,
+                        Vec3<T> &forY) {
+  look = Vec3<T>(camera.focus - camera.position);
+  Normalize(look);
+  Vec3<T> u = Cross(look, camera.up);
+  Normalize(u);
+  Vec3<T> v = Cross(look, u);
+  Normalize(v);
+  T radians = camera.angle * PI / 180.0;
+  T x_multiplier = 2*tan(radians)/ (T)WIDTH;
+  T y_multiplier = 2*tan(radians)/ (T)HEIGHT;
+  forX = Multiply(u, x_multiplier);
+  forY = Multiply(v, y_multiplier);
 }
--- raycasting/raycaster.cxx
+++ raycasting/raycaster.cxx
@@ -1,34 +1,51 @@
 #include "auxiliary.hxx"
+#include "vtkDataSetReader.h"
 
-#define HEIGHT 1000;
-#define WIDTH  1000;
-
-template<typename T>
-void GenerateRay(Vec3<T>& ray, unsigned int pixelX, unsigned int pixelY)
+template <typename T>
+void GenerateRay(Vec3<T> &ray, unsigned int pixelX, unsigned int pixelY,
+                 Vec3<T> look, Vec3<T> forX, Vec3<T> forY)
 {
-
+  T x_multiplier = (2*pixelX + 1 - WIDTH) / (T)2.0;
+  T y_multiplier = (2*pixelY + 1 - HEIGHT) / (T)2.0;
+  Vec3<T> dX = Multiply(forX, x_multiplier);
+  Vec3<T> dY = Multiply(forY, y_multiplier);
+  ray = look + dX + dY;
 }
 
-template<typename T>
-CalculateViewParameters(Camera<T>& camera, Vec3<T>& look, Vec3<T> forX, Vec3<T> forY)
-{
-  look 
-}
+int main(int argc, char **argv) {
 
-int main(int argc, char** argv)
-{
+  if(argc < 3)
+  {
+    std::cout << "usage : raycaster <filename> <variable>" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
   const int height = HEIGHT;
-  const int width  = WIDTH;
+  const int width = WIDTH;
+  const std::string filename(argv[1]);
+  int dims[3];
+
+  // Read in the DataSet, which is a Rectilinear/Uniform Grid
+  vtkDataSetReader *reader = vtkDataSetReader::New();
+  reader->SetFileName(filename.c_str());
+  reader->Update();
+  vtkRectilinearGrid *rgrid = (vtkRectilinearGrid *) rdr->GetOutput();
+  rgrid->GetDimensions(dims);
+
+  double *xCoords = (double*) rgrid->GetXCoordinates()->GetVoidPointer(0);
+  double *yCoords = (double*) rgrid->GetYCoordinates()->GetVoidPointer(0);
+  double *zCoords = (double*) rgrid->GetZCoordinates()->GetVoidPointer(0);
+  double *fieldData = (double*) rgrid->GetPointData()->GetScalars()->GetVoidPointer(0);
 
   Camera<double> camera = SetupCamera<double>();
 
-  Vec3<T> look, forX, forY;
+  Vec3<double> look, forX, forY;
   CalculateViewParameters(camera, look, forX, forY);
 
-  for(size_t pixelX; pixelX < width; ++pixelX)
-    for(size_t pixelY; pixelY < height; ++pixelY)
-    {
+  for (size_t pixelX = 0; pixelX < width; ++pixelX)
+    for (size_t pixelY = 0; pixelY < height; ++pixelY) {
       Vec3<double> ray;
-      GenerateRay(ray, pixelX, pixelY);
+      GenerateRay(ray, pixelX, pixelY, look, forX, forY);
+      Normalize(ray);
     }
 }
